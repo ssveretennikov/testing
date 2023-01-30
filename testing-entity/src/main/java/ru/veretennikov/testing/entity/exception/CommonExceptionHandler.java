60909fe93942dc8863f813fad5679a0d00c3603d
energy 778 <one.million.dollars@mail.ru>
2023-01-30 04:53:16 +0300

    добавлен обработчик для ошибки HttpMessageNotReadableException

--- testing-entity/src/main/java/ru/veretennikov/testing/entity/exception/CommonExceptionHandler.java
+++ testing-entity/src/main/java/ru/veretennikov/testing/entity/exception/CommonExceptionHandler.java
@@ -2,6 +2,7 @@ package ru.veretennikov.testing.entity.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,6 +12,7 @@ import java.time.OffsetDateTime;
 import static java.lang.String.format;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
 @Slf4j
 @RestControllerAdvice
@@ -32,6 +34,20 @@ public class CommonExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Error> handleValidationException(MethodArgumentNotValidException ex) {
         log.error(format("Ошибка валидации: %s", ex.getMessage()), ex);
+        // TODO: подумать над выводом сообщения в более читаемом формате
+        Error error = Error.builder()
+                .message(ex.getMessage())
+                .timestamp(OffsetDateTime.now())
+                .type(ex.getClass().getSimpleName())
+                .build();
+        return ResponseEntity
+                .status(UNPROCESSABLE_ENTITY)
+                .body(error);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Error> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        log.error(format("Ошибка чтения из json: %s", ex.getMessage()), ex);
         Error error = Error.builder()
                 .message(ex.getMessage())
                 .timestamp(OffsetDateTime.now())
